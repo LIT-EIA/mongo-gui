d02b3c3991ea54dc7f3d4f9e8122e3724457a960
Francis Tourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2023-09-03 15:42:41 -0400

    auth for multiple users

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,6 @@
 const base64 = require('base-64');
 require('dotenv').config();
+var configurations = require('../../conf/config.json');
 
 function _decodeCredentials(header) {
   const encode = header.trim().replace(/Basic\s+/i, '');
@@ -11,18 +12,13 @@ function auth(req, res, next) {
   const [username, password] = _decodeCredentials(
     req.headers.authorization || ''
   );
-  if (process.env.MONGOGUI_USERNAME == undefined && process.env.MONGOGUI_PASSWORD == undefined) {
-    return next();
-  } else {
-    if (
-      username === process.env.MONGOGUI_USERNAME &&
-      password === process.env.MONGOGUI_PASSWORD
-    ) {
+    const user = configurations.users.find((user) => user.username === username);
+    if (user && user.password === password) {
       return next();
+    } else {
+      res.set('WWW-Authenticate', 'Basic realm="user_pages"');
+      res.status(401).send('Authentication required!');
     }
-    res.set('WWW-Authenticate', 'Basic realm="user_pages"');
-    res.status(401).send('Authentication required!');
-  }
 }
 
 module.exports = {
